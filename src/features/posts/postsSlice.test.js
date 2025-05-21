4f75813f84392fd67edf0eb9e7b0a6d5824d242e
DevelopingAEUribe <102454811+DevelopingAEUribe@users.noreply.github.com>
2025-05-21 12:24:22 -0400

    Delete src directory

--- src/features/posts/postsSlice.test.js
+++
@@ -1,9 +0,0 @@
-import postsReducer, { votePost } from './postsSlice';
-
-test('votePost increments score', () => {
-  const initialState = {
-    posts: [{ id: '1', score: 10 }],
-  };
-  const result = postsReducer(initialState, votePost({ id: '1', amount: 1 }));
-  expect(result.posts[0].score).toBe(11);
-});
